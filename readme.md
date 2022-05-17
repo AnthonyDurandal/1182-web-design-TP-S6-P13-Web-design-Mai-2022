ca6144437c37f2fd9a8599563ddce15b396ebcbc
AnthonyDurandal <R.I.A.Durandal@outlook.com>
2022-05-17 23:34:00 +0300

    update readme

--- readme.md
+++ readme.md
@@ -1,19 +1,28 @@
-lien vers le site:
-
 projet github:
+    https://github.com/AnthonyDurandal/1182-web-design-TP-S6-P13-Web-design-Mai-2022
 
 lien vers le site:
+    https://tp-s6-web-2022-1182.herokuapp.com/
 
 login:
+    lien: '/admin'
     username:admin@gmail.com
     mdp:mdp
 
 ETU:1182
 
-premier score:
+1-premier score 97:
     ![Alt text](./assets/images/readme-img/scores.png?raw=true "performance score")
 
-optimizations:
+2-optimizations:
     minifier css et js dans un seul fichier(dans public/build/app.css et public/build/app.js )
+        ![Alt text](./assets/images/readme-img/minifier-css.png?raw=true "performance score")
     assemblage images en une seule image
     cache côté serveur (dans le fichier tempates/cache_dir)
+
+
+3-score après optimisation 98:
+    ![Alt text](./assets/images/readme-img/score_apres_optim.png?raw=true "performance score après optimisation")
+    remarques:
+        -après le premier chargement le rendu est plus rapide
+        -le css est minifié
---
+++ templates/cache_dir/32.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+
+		<!-- Meta Tag -->
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta
+		http-equiv="X-UA-Compatible" content="IE=edge">
+
+		<!-- SEO -->
+		
+
+	<meta name="description" content="
+Les &lt;strong&gt;incendies&lt;/strong&gt; australiens, nommés bushfire ou feux de brousse, suscitent un battage médiatique permanent, fondé en grande partie sur une cause ostentatoire et considérée comme une évidence : le « réchauffement climatique ». 
+
+Au-delà de ces annonces le plus souvent teintées d’idéologie, une lecture plus attentive et objective de la réalité révèle une multiplicité de causes que les médias s’abstiennent d’évoquer la plupart du temps ...">
+	<meta name="author" content="durandal">
+<meta name="url" content="https://tp-s6-web-2022-1182.herokuapp.com/accident/incendies-en-australie-accident-rechauffement-climatique_32.html">
+	<meta name="copyright" content="durandal">
+	<meta name="robots" content="index,follow">
+
+
+		<title>
+				incendies-australie-accident-rechauffement-climatique-actu
+		</title>
+
+					<link rel="stylesheet" href="/build/app.css">
+		
+
+	</head>
+
+
+	<body
+		class="body-container">
+
+		
+		<div class="background-wallpaper-container">
+			<img src="/build/images/template-imgs/recyclage-rechauffement-climatique.png" alt="" class="background-wallpaper" style='height:100%;'/>
+
+		</div>
+		<div id="main" style='display:flex;justify-content:center'>
+			<div class="container" style='width: 45vw;'>
+				<div
+					class="row">
+
+					<!-- Blog Post (Right Sidebar) Start -->
+					<div class="col-md-9">
+						<div class="col-md-12 page-body">
+							<div class="row">
+
+
+								<div class="sub-title">
+									<h2 class="green-text">
+											De nombreux incendies en australie
+									</h2>
+									<i class="icon-bubbles" class="green-text"></i>
+								</div>
+
+								<div
+									class="col-md-12 content-page" style="padding-left:20px;padding-right:20px;">
+
+
+									<!-- page content start -->
+									<style>
+	.image-gallery {
+		display: grid;
+		grid-template-columns: 1fr 1fr 1fr;
+		grid-gap: 20px;  
+		width: 640px;
+		height: 300px;
+	}
+
+	.image-gallery div:nth-child(1){
+		background-image: url("/build/images/resized-imgs/catastrophes-rechauffement-climatique.png");
+
+			background-position: 0px 0px;
+	}
+
+	.image-gallery div:nth-child(2){
+		background-image: url("/build/images/resized-imgs/catastrophes-rechauffement-climatique.png");
+		background-position: 200px 0px;
+	}
+
+	.image-gallery div:nth-child(3){
+		background-image: url("/build/images/resized-imgs/catastrophes-rechauffement-climatique.png");
+		background-position: 400px 0px;
+	}
+
+	.gallery-container{
+		width:100%;
+		display:flex;
+		justify-content:center;
+	}
+</style>
+
+
+	<div
+		class="col-md-12 blog-post" style='display:block'>
+
+		<!-- Post Headline Start -->
+		<div class="post-title">
+			<h1>De nombreux incendies en australie</h1>
+		</div>
+		<!-- Post Headline End -->
+
+
+		<!-- Post Detail Start -->
+		<div class="post-info">
+			<span>Article du
+				Jun 2, 1995, 4:37:32 PM
+				écrit par
+				Louis Menard</span>
+		</div>
+		<!-- Post Detail End -->
+
+
+		<p>
+Les <strong>incendies</strong> australiens, nommés bushfire ou feux de brousse, suscitent un battage médiatique permanent, fondé en grande partie sur une cause ostentatoire et considérée comme une évidence : le « réchauffement climatique ».
+
+Au-delà de ces annonces le plus souvent teintées d’idéologie, une lecture plus attentive et objective de la réalité révèle une multiplicité de causes que les médias s’abstiennent d’évoquer la plupart du temps.
+
+En 2017, j’avais eu l’occasion d’écrire dans les lignes de Contrepoints l’amalgame qui était fait entre les importants <strong>incendies</strong> dans le Sud-Est, et l’interprétation de leur origine… à savoir l’indéniable « réchauffement climatique anthropique ».
+
+De la même façon, je vous propose une lecture objective de la problématique et très synthétique, car le sujet est vaste, des <strong>incendies australiens</strong>.
+
+Des <strong>incendies</strong> majeurs
+On ne peut nier la catastrophe que vit l’<strong>Australie</strong> depuis deux ans, puisque ces <strong>incendies</strong> majeurs ont débuté en 2018. Ils ne sont pas tous situés dans l’Est australien, mais également dans le Nord-Est, le Nord, et le Sud-Ouest, les plus importants se trouvant néanmoins en janvier 2020 dans la partie est des Nouvelles-Galles du Sud.
+
+Il s’agit d’un phénomène naturel extrême, amplifié par les conditions météorologiques, ainsi que par d’autres causes que nous allons examiner.
+                </p>
+
+
+		<!-- Post Image Start -->
+		<div class="post-image margin-top-40 margin-bottom-40">
+
+		<img src="/build2/images/ours-glacier-fonte-rechauffement-climatique.jpg" alt="ours-sur-un-glacier-en-fonte-rechauffement-climatique.jpg">
+
+			<p>Aidez nous à sauver notre chère planète.</p>
+		</div>
+		<!-- Post Image End -->
+
+		<!-- Post Image Gallery Start -->
+		<div class="gallery-container">
+			<div class="image-gallery">
+				<div class="image-gallery-element"></div>
+				<div class="image-gallery-element"></div>
+				<div class="image-gallery-element"></div>
+			</div>
+		</div>
+		<!-- Post Image Gallery End -->
+	</div>
+	<!-- Post Author Bio Box ends -->
+
+</div>									<!-- page content end -->
+
+								<!-- Load button Start -->
+
+									<!-- Load button Ends -->
+
+								</div>
+
+							</div>
+
+						</div>
+
+
+						<!-- Footer Start -->
+						<div class="col-md-12 page-body margin-top-50 footer">
+							<footer></footer>
+						</div>
+						<!-- Footer End -->
+
+
+					</div>
+					<!-- Blog Post (Right Sidebar) End -->
+
+				</div>
+			</div>
+		</div>
+
+
+		<!-- Back to Top Start -->
+		<a href="#" class="scroll-to-top">
+			<i class="fa fa-long-arrow-up"></i>
+		</a>
+		<!-- Back to Top End -->
+	</body>
+</html></body></html><!-- All Javascript Plugins  --><script src="/build/runtime.js" defer></script><script src="/build/app.js" defer></script>
