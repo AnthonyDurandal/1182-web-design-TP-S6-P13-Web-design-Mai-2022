4ee39b41399b5dc6eb7f5db4064fce4df26c2004
AnthonyDurandal <R.I.A.Durandal@outlook.com>
2022-05-17 12:42:43 +0300

    remove js lib

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -5,7 +5,7 @@
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '5251cea20b5689ab55b91553532a718579ce07ba',
+        'reference' => 'c5b041b4631d8def25a11c9988db08d3aec1fd94',
         'name' => '__root__',
         'dev' => true,
     ),
@@ -16,7 +16,7 @@
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => '5251cea20b5689ab55b91553532a718579ce07ba',
+            'reference' => 'c5b041b4631d8def25a11c9988db08d3aec1fd94',
             'dev_requirement' => false,
         ),
         'doctrine/annotations' => array(
@@ -357,6 +357,15 @@
             'install_path' => __DIR__ . '/../sensio/framework-extra-bundle',
             'aliases' => array(),
             'reference' => '6bd976c99ef3f78e31c9490a10ba6dd8901076eb',
+            'dev_requirement' => false,
+        ),
+        'symfony/apache-pack' => array(
+            'pretty_version' => 'v1.0.1',
+            'version' => '1.0.1.0',
+            'type' => 'symfony-pack',
+            'install_path' => __DIR__ . '/../symfony/apache-pack',
+            'aliases' => array(),
+            'reference' => '3aa5818d73ad2551281fc58a75afd9ca82622e6c',
             'dev_requirement' => false,
         ),
         'symfony/asset' => array(
